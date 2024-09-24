be799d50988bb0cc7908a76ac2b3a26ee4bda1fa
DreamDaeWon@NoteBookOmen <june25656@gmail.com>
2024-09-24 10:19:57 +0900

    원 운동 그리기 완료

--- character_grass.py
+++ character_grass.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import math
 
 
 open_canvas()
@@ -20,9 +20,15 @@ def run_rectangle():
 def run_circle():
     print('Circle')
 
-    clear_canvas_now()
-    boy.draw_now(400,30)
-    delay(1)
+    r, cx, cy  = 300, 800//2, 600//2
+
+    for d in range(0,360):
+        x = cx + r * math.cos(math.radians(d))
+        y = cy + r * math.sin(math.radians(d))
+        clear_canvas_now()
+        boy.draw_now(x, y)
+        delay(0.01)
+
 
     pass
 
