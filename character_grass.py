536810d44dcf693e7211265cc394aa01870426be
DreamDaeWon@NoteBookOmen <june25656@gmail.com>
2024-09-24 10:01:54 +0900

    캐릭터 출력 성공

--- character_grass.py
+++ character_grass.py
@@ -1,12 +1,6 @@
 from pico2d import *
 
-def run_rectangle():
-    print('rectangle')
-    pass
 
-def run_circle():
-    print('Circle')
-    pass
 
 open_canvas()
 
@@ -16,10 +10,26 @@ grass = load_image('grass.png')
 
 boy = load_image('character.png')
 
-while True:
 
+def run_rectangle():
+    print('rectangle')
+
+    pass
+
+
+def run_circle():
+    print('Circle')
+
+    clear_canvas_now()
+    boy.draw_now(400,30)
+    delay(1)
+
+    pass
+
+
+while True:
     run_rectangle()
     run_circle()
-
+    break
 
 close_canvas()
